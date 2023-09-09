aa2f8c9ccd75d65c29e60fd1079bf4fb6e2251f1
c8e4 <c8e4@proton.me>
2023-09-09 17:06:55 +0200

    visualize tiles

---
+++ src/lib/SmallTile.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let val:null|string=null;
+</script>
+<div
+    class="small-tile"
+    class:bg-green-200={val?.startsWith("p")}
+    class:bg-slate-300={val?.startsWith("d")}
+    class:bg-yellow-300={val?.startsWith("z")}
+>
+    {val ?? ""}
+</div>
+
+<style>
+    .small-tile {
+        width: 50px;
+        height: 50px;
+        border: 1px solid gray;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,48 @@
 <script lang="ts">
+    import SmallTile from "$lib/SmallTile.svelte";
     import { tiles } from "$lib/game/tiles";
 </script>
 
+<div class="flex flex-wrap gap-4">
 {#each tiles as tile}
-    <div>{tile.name}</div>
+<div>
+    <div class="flex">
+        <SmallTile></SmallTile>
+        <SmallTile val={tile.connectors[11]}></SmallTile>
+        <SmallTile val={tile.connectors[0]}></SmallTile>
+        <SmallTile val={tile.connectors[1]}></SmallTile>
+        <SmallTile></SmallTile>
+    </div>
+    <div class="flex">
+        <div class="">
+            <SmallTile val={tile.connectors[10]}></SmallTile>
+            <SmallTile val={tile.connectors[9]}></SmallTile>
+            <SmallTile val={tile.connectors[8]}></SmallTile>
+        </div>
+        <div class="tile-center bg-gray-200 flex items-center justify-center text-3xl font-bold">
+            {tile.name}
+        </div>
+        <div class="">
+            <SmallTile val={tile.connectors[2]}></SmallTile>
+            <SmallTile val={tile.connectors[3]}></SmallTile>
+            <SmallTile val={tile.connectors[4]}></SmallTile>
+        </div>
+    </div>
+    <div class="flex">
+        <SmallTile></SmallTile>
+        <SmallTile val={tile.connectors[5]}></SmallTile>
+        <SmallTile val={tile.connectors[6]}></SmallTile>
+        <SmallTile val={tile.connectors[7]}></SmallTile>
+        <SmallTile></SmallTile>
+    </div>
+</div>
 {/each}
+</div>
+
+<style>
+    .tile-center{
+        width: 150px;
+        height:150px;
+        border: 1px solid gray;
+    }
+</style>
