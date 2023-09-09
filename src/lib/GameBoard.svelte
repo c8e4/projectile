e65e804c331de03b239aca1754aa2ece6e5216e2
Savonarola <SavonarolaX@proton.me>
2023-09-09 23:02:03 +0400

    Tile placement

--- src/lib/GameBoard.svelte
+++ src/lib/GameBoard.svelte
@@ -15,7 +15,11 @@
                 coord: toVisualGridCoordinates(i, j),
                 tile: {
                     name: "",
+                    deg: 0,
                 },
+                x: i,
+                y: j,
+                locked: false,
             };
         }
     }
@@ -23,24 +27,113 @@
         name: "D",
         deg: 0,
     };
-    grid[40][41].tile = {
-        name: "A",
-    };
+    grid[40][40].locked = true;
+    /*grid[40][41].tile = {
+    name: "C",
+  };
+  */
+
+    let activeCell = null;
 
     onMount(() => {
         window.scrollTo(4000 - 100 * 5, 4000 - 100 * 4);
     });
 
     function toVisualGridCoordinates(x, y) {
-        return `${y - GRID_CENTER}, ${(x - GRID_CENTER) * -1}`;
+        return `${x},${y}`; //`${y - GRID_CENTER}, ${(x - GRID_CENTER) * -1}`;
     }
 
     function landTileAtCell(cell) {
+        if (!hasNeighbours(cell)) {
+            return;
+        }
+        if (activeCell) {
+            grid[activeCell.x][activeCell.y].tile = {
+                name: "",
+                deg: 0,
+            };
+            activeCell = null;
+        }
+
+        console.log("old cell", activeCell);
+        activeCell = cell;
+        console.log("New", activeCell);
         cell.tile = {
             name: "B",
         };
         grid = grid;
+        //activeCell = activeCell;
     }
+
+    function hasNeighbours(cell) {
+        const x = cell.x;
+        const y = cell.y;
+        //neighbours
+        if (x + 1 < GRID_SIZE) {
+            if (grid[x + 1][y].locked) {
+                return true;
+            }
+        }
+        if (y + 1 < GRID_SIZE) {
+            if (grid[x][y + 1].locked) {
+                return true;
+            }
+        }
+        if (x - 1 >= 0) {
+            if (grid[x - 1][y].locked) {
+                return true;
+            }
+        }
+        if (y - 1 >= 0) {
+            if (grid[x][y - 1].locked) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function onKeyDown(e) {
+        console.log(e.key);
+        if (e.key == "r") {
+            //
+            rotateActiveCell(activeCell);
+            grid = grid;
+        }
+        if (e.key == " ") {
+            confirmTilePlacement(activeCell);
+            console.log("this is probel");
+        }
+    }
+
+    function rotateActiveCell(cell) {
+        if (!cell) {
+            return;
+        }
+        if (!cell.tile.deg) {
+            cell.tile.deg = 90;
+        } else {
+            cell.tile.deg = (cell.tile.deg + 90) % 360;
+        }
+        grid = grid;
+    }
+
+    function confirmTilePlacement() {
+        if (!activeCell) {
+            return;
+        }
+        // 1 Check Connectors
+        activeCell.locked = true;
+        activeCell = null;
+        // 3 Next Step "BadBoyPlacement"
+        // 4 Calculate if Castle/Road/Church completed?
+    }
+
+    function getNextTile() {
+        // 1 Not enough tiles ?
+        // 2 Calculate winner
+        // 3 New Random Tile
+    }
+
 </script>
 
 <div class="board bg-green-100">
@@ -66,6 +159,8 @@
         </div>
     {/each}
 </div>
+
+<svelte:window on:keydown|preventDefault={onKeyDown} />
 
 <style>
     .board {
