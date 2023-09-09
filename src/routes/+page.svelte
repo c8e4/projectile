2c90206061cba111b072fd03cb1a6d30291bbbd0
c8e4 <c8e4@proton.me>
2023-09-09 23:24:17 +0200

    add three.js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     import GameBoard from "$lib/GameBoard.svelte";
+    import Three from "$lib/Three.svelte";
     import TilePreview from "$lib/TilePreview.svelte";
 </script>
 <!-- <TilePreview></TilePreview> -->
-<GameBoard></GameBoard>
+<!-- <GameBoard></GameBoard> -->
+<Three></Three>
