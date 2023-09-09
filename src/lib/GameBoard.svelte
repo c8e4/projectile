f6cb25e32bd13832af6f2445fb35477ba5aa4943
c8e4 <c8e4@proton.me>
2023-09-09 18:59:14 +0200

    create a basic grid

---
+++ src/lib/GameBoard.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import SmallTile from "$lib/SmallTile.svelte";
+    import { tiles } from "$lib/game/tiles";
+    import { onMount } from "svelte";
+
+    let GRID_SIZE = 81;
+    let GRID_CENTER = 40;
+    let grid =  new Array(GRID_SIZE).fill(null).map(x => new Array(GRID_SIZE).fill(null))
+    
+    for(let i=0; i <GRID_SIZE; i++){
+        for(let j=0; j <GRID_SIZE; j++){
+            grid[i][j]= toVisualGridCoordinates(i,j)
+        }
+    }
+
+    onMount(()=>{
+        console.log(grid)
+        window.scrollTo(4000-100*5,4000-100*4);
+    })
+
+    function toVisualGridCoordinates(x,y){
+        return `${y-GRID_CENTER}, ${(x-GRID_CENTER)*-1}`
+    }
+</script>
+
+<div class="board bg-green-100">
+{#each grid as row}
+<div class="flex">
+    {#each row as cell}
+    <div class="cell bg-red-100">{cell}</div>
+    {/each}
+</div>
+{/each}
+</div>
+
+<style>
+.board{
+    width: 8000px;
+    height: 8000px;
+}
+.cell{
+    width:100px;
+    height:100px;
+    border: 1px solid gray;
+    display: flex;
+    align-items:center;
+    justify-content: center;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,6 @@
-<script lang="ts">
-    import SmallTile from "$lib/SmallTile.svelte";
-    import { tiles } from "$lib/game/tiles";
-    let myTiles = tiles 
-    
-    function rotateTile(tile){
-        for (let i = 0 ; i<3 ; i++ ) {
-            tile.connectors.unshift(tile.connectors[tile.connectors.length-1]);
-            tile.connectors.pop();
-        }
-        myTiles=myTiles
-    }
-
+<script>
+    import GameBoard from "$lib/GameBoard.svelte";
+    import TilePreview from "$lib/TilePreview.svelte";
 </script>
-
-<div class="flex flex-wrap gap-4">
-{#each myTiles as tile}
-<div>
-    <div class="flex">
-        <SmallTile></SmallTile>
-        <SmallTile val={tile.connectors[11]}></SmallTile>
-        <SmallTile val={tile.connectors[0]}></SmallTile>
-        <SmallTile val={tile.connectors[1]}></SmallTile>
-        <SmallTile></SmallTile>
-    </div>
-    <div class="flex">
-        <div class="">
-            <SmallTile val={tile.connectors[10]}></SmallTile>
-            <SmallTile val={tile.connectors[9]}></SmallTile>
-            <SmallTile val={tile.connectors[8]}></SmallTile>
-        </div>
-        <div class="tile-center bg-gray-200 flex items-center justify-center text-3xl font-bold" on:click={()=>rotateTile(tile)}>
-            {tile.name}
-        </div>
-        <div class="">
-            <SmallTile val={tile.connectors[2]}></SmallTile>
-            <SmallTile val={tile.connectors[3]}></SmallTile>
-            <SmallTile val={tile.connectors[4]}></SmallTile>
-        </div>
-    </div>
-    <div class="flex">
-        <SmallTile></SmallTile>
-        <SmallTile val={tile.connectors[7]}></SmallTile>
-        <SmallTile val={tile.connectors[6]}></SmallTile>
-        <SmallTile val={tile.connectors[5]}></SmallTile>
-        <SmallTile></SmallTile>
-    </div>
-</div>
-{/each}
-</div>
-
-<style>
-    .tile-center{
-        width: 150px;
-        height:150px;
-        border: 1px solid gray;
-    }
-</style>
+<!-- <TilePreview></TilePreview> -->
+<GameBoard></GameBoard>
