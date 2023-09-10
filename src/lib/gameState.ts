2d4744a43486bc6a17e5c97d396e56bba690042c
c8e4 <c8e4@proton.me>
2023-09-10 13:44:43 +0200

    add game state types

--- src/lib/gameState.ts
+++ src/lib/gameState.ts
@@ -1,6 +1,31 @@
-export const gameState = {
+type TileConnector = string | null
+
+type TileConnectorList = Array<TileConnector>
+
+type FieldTile = {
+  coord: string
+  tile: {
+    name: string
+    deg: number
+    connectors: TileConnectorList
+    center: string | null
+  }
+  x: number
+  y: number
+  locked: boolean
+}
+
+type GameState = {
+  tiles: Array<FieldTile>
+}
+
+export const GRID_SIZE = 81;
+export const GRID_CENTER = 40;
+
+export const gameState:GameState = {
   //tiles
   //
   //players
   //  
+  tiles: []
 } 
